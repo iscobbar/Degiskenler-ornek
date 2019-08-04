c509b834f7a56caba19caa21ebd1e5eb9c434f58
İskender Gürün <iskender@hotmail.com>
2019-08-04 17:51:10 +0300

    Add project files.

---
+++ ConsoleApplication16/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tek satır açıklama / yorum / comment
+
+            /*
+             * Çoklu
+             * Açıklama satırı
+             * 
+             */
+
+             //Değer Tipi Değişkenler*******************************
+             //Tip Ad = Veri;
+
+            sbyte sayi = -56;
+            Console.WriteLine(sayi);
+            sayi = 56;
+            Console.WriteLine(sayi);
+            byte sayi1 = 45;
+            short sayi2 = 32000;
+            int sayi3 = 45000;
+            int sayi4 = sayi3;
+            Console.WriteLine(sayi4);
+            long say5 = 123123123123;
+            float pi = 3.14f;
+            float pi2 = 3.14F;
+            float enBuyukFloat = float.MaxValue;
+            float enKucukFloat = float.MinValue;
+
+            Console.WriteLine("En Büyük Float: " + enBuyukFloat);
+            Console.WriteLine("En Küçük Float: " + enKucukFloat);
+            
+            double ondalikli1 = 4.55;
+            double ondalikli2 = 5.33D;
+            double ondalikli3 = 6.55d;
+
+            decimal buyukOndalikli = 33333.5555M;
+            decimal buyukOndalikli2 = 33333.5555m;
+
+            bool kontrol = true;
+            bool kontrol2 = false;
+            
+            char harf = 'F';
+            char harf2 = '4';
+            char harf3 = 'V';
+
+            string isim = "Abdullah Syeda AKSAKAL";
+            Console.WriteLine(isim);
+
+            string finAlly = "Mehmet DEMİR";
+
+            int SAYI = 5;
+            int sayi = 6;
+            Console.WriteLine(SAYI + sayi);
+            
+
+            //Otomatik tip belirleme(değer tipi)
+
+            //var değer tipinde bir veri.
+            var s1 = 55;
+
+            var d1 = 5.5;
+
+            // Program çalıştığında int s1 = 55;
+
+
+            var s2 = "Merhaba";
+            ////Program çalıştığında string s2 = "Merhaba";
+            var s3 = true;
+            var s4 = 'F';
+            s4 = 'E';
+
+            //harf = 5; Burada char tipi değer tipinde olduğu için farklı türden bir veriyi kabul emteyecek.
+
+
+
+            Console.WriteLine("Smartpro Bilgisayar Akademisi");
+            Console.WriteLine(sayi3);
+
+
+            Console.Write("Merhaba");
+            Console.Write(" ");
+            Console.Write("Dünya");
+
+
+            Console.Write("Merhaba" + " " + "Dünya");
+
+            Console.WriteLine("Merhaba");
+            Console.WriteLine("Dünya");
+
+            byte yas = 55;
+            short maas = 4000;
+            string isim = "Necdet TUNA";
+
+            Console.WriteLine(isim + " " + yas + " " + maas);
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Maaş : " + maas + " Yaş: " + yas);
+
+            Console.WriteLine("Sayi3 değişkeninin değeri: " + sayi3);
+            Console.WriteLine("Sayi2 değişkeninin değeri: " + sayi2 + " Sayi1 değişkenin değer: " + sayi1);
+
+            Console.WriteLine(sayi1 + sayi2);    // 32045
+            Console.WriteLine(sayi1 + "" + sayi2); // 4532000
+
+
+
+            // Referans Tipi Veriler.
+
+            /*
+                Ram'deki adresleri tutar.
+             */
+
+
+            object turlu = 88;
+
+            turlu = "ASD";
+
+
+
+
+            object degisken = 55;
+            degisken = "Metinsel İfade";
+
+            String degisken2 = "Merhaba";
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
